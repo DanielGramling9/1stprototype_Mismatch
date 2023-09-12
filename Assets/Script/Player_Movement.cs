875653ef78b505778db5601e49e0137b363f9611
DanielGramling9 <112036402+DanielGramling9@users.noreply.github.com>
2023-09-12 11:16:05 -0400

    WASD Movement
    The movement keys WASD makes the character move. Very fact however. Change speed using Movement_speed variable.

--- Assets/Script/Player_Movement.cs
+++ Assets/Script/Player_Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    [SerializeField]
+    private Transform Player;
+    [SerializeField]
+    private float Movement_speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +17,51 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
             print("You are pressing W!");
+            float x = Player.localPosition.x + Movement_speed* Time.deltaTime;
+            float y = (Player.localPosition.y) ;
+            float z = (Player.localPosition.z ) ;
+            Vector3 pos = new Vector3(x,y, z);
+            Player.position =pos;
+            print(" X: " + x + " Z: " + z);
+           // print("PlayerPosition" + pos);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("You are pressing S!");
+            float x = Player.localPosition.x - Movement_speed * Time.deltaTime;
+            float y = (Player.localPosition.y);
+            float z = (Player.localPosition.z);
+            Vector3 pos = new Vector3(x, y, z);
+            Player.position = pos;
+           // print(" X: " + x + " Z: " + z);
+            //print("PlayerPosition" + pos);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("You are pressing A!");
+            float x = Player.localPosition.x;
+            float y = (Player.localPosition.y);
+            float z = (Player.localPosition.z + Movement_speed * Time.deltaTime);
+            Vector3 pos = new Vector3(x, y, z);
+            Player.position = pos;
+            // print(" X: " + x + " Z: " + z);
+            //print("PlayerPosition" + pos);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("You are pressing D!");
+            float x = Player.localPosition.x ;
+            float y = (Player.localPosition.y);
+            float z = (Player.localPosition.z) - Movement_speed * Time.deltaTime;
+            Vector3 pos = new Vector3(x, y, z);
+            Player.position = pos;
+            // print(" X: " + x + " Z: " + z);
+            //print("PlayerPosition" + pos);
         }
     }
 }
